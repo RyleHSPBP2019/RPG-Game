0b1fc017b31fd77532a0fa624c406637d3b9917a
lasch <lasch@LAPTOP-5BJ49K1S.BOONE.ketsds.net>
2019-09-26 14:26:39 -0400

    spawn calculation

--- src/Characters/Monster/Monster.java
+++ src/Characters/Monster/Monster.java
@@ -1,23 +1,14 @@
 package Characters.Monster;
 
-public class Monster {
+public abstract class Monster {
 	private double damage;
 	private double armorPenPercent; // armor pens works as so: (for every amount of damage, the penetration value is a certain percentage of that added on)
 	private int itemDrop; // drops are inside an array, so we call a number instead of an object
 	private double itemDropChance; // ex. (30% = 30.0)
-	private double spawnRate;
+	private double spawnRate; // this is also a percentage value (30% = 30.0)
 	private int health;
 	
-	// constructor methods
-	
-	public Monster() {
-		damage = 5.0;
-		armorPenPercent = 10.0;
-		itemDrop = 0;
-		itemDropChance = 30.0;
-		spawnRate = 1.0;
-		health = 100;
-	}
+	// constructor method
 	
 	public Monster(double dmg, double armPP, int itmDrp, 
 				   double itmDrpChnc, double spwnR, int hlth) {
@@ -29,5 +20,6 @@ public class Monster {
 		health = hlth;
 	}
 	
+	public abstract int calculateSpawn(int roomNumber, int toughNum, int difficulty);
 	
 }
--- src/Characters/Monster/Wolf.java
+++ src/Characters/Monster/Wolf.java
@@ -2,13 +2,23 @@ package Characters.Monster;
 
 public class Wolf extends Monster {
 	
-	public Wolf() {
-		
-	}
-
 	public Wolf(double dmg, double armPP, int itmDrp, double itmDrpChnc, double spwnR, int hlth) {
 		super(dmg, armPP, itmDrp, itmDrpChnc, spwnR, hlth);
-		
+		dmg = 10.0;
+		armPP = 10.0;
+		itmDrp = 0;
+		itmDrpChnc = 30.0;
+		spwnR = 0;
+		hlth = 100;
+	}
+
+	@Override
+	public int calculateSpawn(int roomNumber, int toughNum, int difficulty) {
+		int randNum = (int)Math.random() * 10;
+		double roomStrength = (100.0)/(1 + difficulty * Math.pow(Math.E, -0.2 * roomNumber));
+		double strengthRoll = randNum * roomStrength;
+		// if statement here
+		return 0;
 	}
 
 }
