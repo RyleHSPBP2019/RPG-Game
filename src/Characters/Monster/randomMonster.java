a11395e57636d8d7cf7f29f24e979f4d30442025
timothy.ketron <timothy.ketron@PC071-L118-11.boone.ketsds.net>
2019-10-01 14:02:27 -0400

    making wolves

--- src/Characters/Monster/randomMonster.java
+++ src/Characters/Monster/randomMonster.java
@@ -1,13 +1,23 @@
 package Characters.Monster;
 
+import java.util.Random;
+
 public class randomMonster {
-	int avgStr[] = new int[16];
+	public void run()
+	{	
+	Random rand = new Random();
+	double avgStr[] = new double[16];
 	Wolf wolves[] = new Wolf[100];
-	for (int i =0; i < 101; i++)
+	for (int i = 0; i < 100; i++) 
 	{
 		double dmg, armPP;
 		int drop, hp;
-		wolves[i] = new Wolf(dmg, armPP,drop,hp);
+		dmg = rand.nextInt(32) + rand.nextDouble();
+		armPP = rand.nextInt(32) + rand.nextDouble();
+		drop = rand.nextInt(32);
+		hp = rand.nextInt(32);
+		wolves[i] = new Wolf(dmg, armPP, drop, hp);
+		avgStr[1] += wolves[i].getPowerLevel();
 	}
-
+	}
 }
