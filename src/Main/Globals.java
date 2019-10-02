e0deffbfa013442671110bac0c0462fa24b965b9
lasch <lasch@LAPTOP-5BJ49K1S.BOONE.ketsds.net>
2019-10-02 13:29:24 -0400

    beamed

---
+++ src/Main/Globals.java
@@ -0,0 +1,6 @@
+package Main;
+
+public class Globals {
+	public static int difficulty;
+	
+}
--- src/Main/Player.java
+++ src/Main/Player.java
@@ -1,7 +1,22 @@
 package Main;
+import Items.Clothes.Boots;
+import Items.Clothes.Chestplate;
+import Items.Clothes.Gauntlets;
+import Items.Clothes.Helmet;
+import Items.Clothes.Leggings;
 
 public class Player {
-
+	private Boots boots;
+	private Chestplate chestplate;
+	private Gauntlets gauntlets;
+	private Helmet helmet;
+	private Leggings leggings;
+	
+	private int health;
+	private double damage;
+	private double critChance;
+	
+	// constructor method
 	public Player() {	
 		
 	}
