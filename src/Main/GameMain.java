3aa707d65f7783715e68086362fb223a30eee304
timothy.ketron <timothy.ketron@PC071-L118-11.boone.ketsds.net>
2019-10-03 13:31:13 -0400

    made it an applet

--- src/Main/GameMain.java
+++ src/Main/GameMain.java
@@ -1,9 +1,31 @@
 package Main;
 
-public class GameMain {
-	
-	public static void main(String[] args) {
+import java.applet.Applet;
+import java.awt.*;
 
+
+
+
+	
+public class GameMain extends Applet
+{
+	
+	Frame f;
+	public void init() {
+	f = new Frames("Game Title");
+	f.setSize(1920, 1080);
+	f.setVisible(true);
 	}
+	public void start() {
+	f.setVisible(true);
+	}
+	public void stop() {
+	f.setVisible(false);
+	}
+	public void paint(Graphics g) {
+	g.drawString("Welcome to te game", 50, 90);
+	}
+
 	
 }
+
