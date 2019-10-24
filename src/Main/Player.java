28b48b7b47f04c8e65ae69fbcf4b3e8bb56bfb55
lasch <lasch@LAPTOP-5BJ49K1S.BOONE.ketsds.net>
2019-10-24 13:13:34 -0400

    finished player

--- src/Main/Player.java
+++ src/Main/Player.java
@@ -15,22 +15,61 @@ public class Player {
 	
 	// player values
 	private int health;
+	private int maxHealth;
 	private int armorAmount;
 	private double damage;
 	
 	// constructor methods
 	
 	public Player() {	// default player makes everything default
-		health = 100;
+		maxHealth = 100;
+		health = maxHealth;
+		armorAmount = (boots.armorValue() + chestplate.armorValue() + gauntlets.armorValue() + helmet.armorValue() + leggings.armorValue());
 		damage = weapon.getDamage();
-		
+		// weapon = #finish here keeton
 	}
 	
 	
 	// adjustment methods
 	
 	public void heal(int amount) {
-		health += amount;
+		if (health != maxHealth) {
+			int healAmountNeeded = maxHealth - health;
+			if (healAmountNeeded >= amount) 
+				health += amount;
+			else if (healAmountNeeded < amount) {
+				amount = healAmountNeeded;
+				health += amount;
+			}
+		}
 	}
 	
+	public void changeWeapon(Weapon newWeapon) {
+		weapon = newWeapon;
+	}
+	
+	public void changeMaxHealthAmount(int amount) { // this is a character setting method, meaning health will be reset as well
+		maxHealth = amount;
+		health = maxHealth;
+	}
+	
+	public void changeBoots(Boots armorBoots) {
+		boots = armorBoots;
+	}
+	
+	public void changeChestplate(Chestplate armorChestplate) {
+		chestplate = armorChestplate;
+	}
+	
+	public void changeGauntlets(Gauntlets armorGauntlets) {
+		gauntlets = armorGauntlets;
+	}
+	
+	public void changeHelmet(Helmet armorHelmet) {
+		helmet = armorHelmet;
+	}
+	
+	public void changeLeggings(Leggings armorLeggings) {
+		leggings = armorLeggings;
+	}
 }
